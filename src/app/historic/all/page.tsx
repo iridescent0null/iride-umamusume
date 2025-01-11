973e95661ab16d3506a30ff3093934e383597537
iridescent0null <mitome19+dev@gmail.com>
2025-01-11 16:37:53 +0900

    considering red factors

--- src/app/historic/all/page.tsx
+++ src/app/historic/all/page.tsx
@@ -22,31 +22,93 @@ const ViewAllHistoricUma = () => {
     const [conditionFourRank, setConditionFourRank] = useState<number>();
     const [conditionFourKey, setConditionFourKey] = useState<UmaPropertyKey | "">();
 
+    const [fatherRedFactorKey,setFatherRedFactorKey] = useState<UmaPropertyKey | "">();
+    const [fatherRedFactorLevel,setFatherRedFactorLevel] = useState<number>(0);
+    const [motherRedFactorKey,setMotherRedFactorKey] = useState<UmaPropertyKey | "">();
+    const [motherRedFactorLevel,setMotherRedFactorLevel] = useState<number>(0);
+
+    /** 
+     * determine how it looses the limitation considering red factors. \
+     * e.g. returning 2 means that C rank is OK when A rank is required, because the following eq is true: \
+     * C <= A - 2. \
+     * When none of the red factors is concerning the given key, just returns 0.
+     */
+    const determineEaseValue = (key: UmaPropertyKey) => {
+        const starMax = 10;
+        let stars = 0;
+        if (fatherRedFactorKey === key) {
+            stars = stars + fatherRedFactorLevel;
+        }
+        if (motherRedFactorKey === key) {
+            stars = stars + motherRedFactorLevel;
+        }
+        if (stars > starMax) {
+            stars = starMax;
+        }
+        return Math.trunc((stars+2)/3); // e.g. 4, 5 and 6 stars have to have the same effect (+2 ranks)
+    };
+
+    // FIXME the A rank cap is working but has been implemented by a little tricky method
     const dependingOnConditionOne = (uma: HistoricUma) => {
         // if there is no valid conditions, all umas are OK
-        if (conditionOneRank === void 0 || Number.isNaN(conditionOneRank)|| !conditionOneKey) {
+        if (conditionOneRank === void 0 || Number.isNaN(conditionOneRank) || !conditionOneKey) {
             return true;
         }
-        return uma.property![conditionOneKey]! <= conditionOneRank;
+        if (conditionOneRank === 0) { 
+            // if S is required, no uma is OK
+            return false;
+        }
+        return uma.property![conditionOneKey]! <= conditionOneRank + determineEaseValue(conditionOneKey);
     }
     const dependingOnConditionTwo = (uma: HistoricUma) => {
-        if (conditionTwoRank === void 0 || Number.isNaN(conditionTwoRank)|| !conditionTwoKey) {
+        if (conditionTwoRank === void 0 || Number.isNaN(conditionTwoRank) || !conditionTwoKey) {
             return true;
         }
-        return uma.property![conditionTwoKey]! <= conditionTwoRank;
+        if (conditionTwoRank === 0) {
+            return false;
+        }
+        return uma.property![conditionTwoKey]! <= conditionTwoRank + determineEaseValue(conditionTwoKey);
     }
     const dependingOnConditionThree = (uma: HistoricUma) => {
-        if (conditionThreeRank === void 0 || Number.isNaN(conditionThreeRank)|| !conditionThreeKey) {
+        if (conditionThreeRank === void 0 || Number.isNaN(conditionThreeRank) || !conditionThreeKey) {
             return true;
         }
-        return uma.property![conditionThreeKey]! <= conditionThreeRank;
+        if (conditionThreeRank === 0) {
+            return false;
+        }
+        return uma.property![conditionThreeKey]! <= conditionThreeRank + determineEaseValue(conditionThreeKey);
     }
     const dependingOnConditionFour = (uma: HistoricUma) => {
-        if (conditionFourRank === void 0 || Number.isNaN(conditionFourRank)|| !conditionFourKey) {
+        if (conditionFourRank === void 0 || Number.isNaN(conditionFourRank) || !conditionFourKey) {
             return true;
         }
-        return uma.property![conditionFourKey]! <= conditionFourRank;
+        if (conditionFourRank === 0) {
+            return false;
+        }
+        return uma.property![conditionFourKey]! <= conditionFourRank + determineEaseValue(conditionFourKey);
     }
+
+    const adoptIngaPreset = () => {
+        (document.getElementById("historic-condition-one-rank") as HTMLSelectElement).value = "1";
+        setConditionOneRank(1);
+        (document.getElementById("historic-condition-one-key") as HTMLSelectElement).value = "turf";
+        setConditionOneKey("turf");
+        (document.getElementById("historic-condition-two-rank") as HTMLSelectElement).value = "3";
+        setConditionTwoRank(3);
+        (document.getElementById("historic-condition-two-key") as HTMLSelectElement).value = "dirt";
+        setConditionTwoKey("dirt");
+        (document.getElementById("historic-condition-three-rank") as HTMLSelectElement).value = "2";
+        setConditionThreeRank(2);
+        (document.getElementById("historic-condition-three-key") as HTMLSelectElement).value = "intermediate";
+        setConditionThreeKey("intermediate");
+        (document.getElementById("historic-condition-four-rank") as HTMLSelectElement).value = "2";
+        setConditionFourRank(2);
+        (document.getElementById("historic-condition-four-key") as HTMLSelectElement).value = "mile";
+        setConditionFourKey("mile");
+
+        (document.getElementById("father-red-factor-key") as HTMLSelectElement).value = "dirt";
+        setFatherRedFactorKey("dirt");
+    };
 
     useEffect(() => {
         const hydrete = () => {
@@ -79,6 +141,60 @@ const ViewAllHistoricUma = () => {
     
         return <div className="dynamic-table">
             <div className="dynamic-search-input">
+                <div className="red-factor-input-wrapper">
+                    <label htmlFor="father-red-factor">father: </label>
+                    <div id="father-red-factor" className="red-factor-input">
+                        <select id="father-red-factor-key" onChange={event => setFatherRedFactorKey(event.target.value as UmaPropertyKey | "")}> 
+                            <option value="">-</option>
+                            {getUmaPropertyKeys().map(key=>
+                                <option value={codeUmaPropertyKey(key)} key={`father-option-${key}`}>
+                                    {key}
+                                </option>
+                            )}
+                        </select>
+                            <span className="three-stars">
+                            <span className="the-0-star star"></span>
+                            {[1,2,3,4,5,6,7,8,9].map(i => <span className={"the-"+i+"-star star"} key={"father-star-"+i}>
+                                ★
+                            </span>)
+                            }
+                            <span className="the-10-star star" key="father-star-10">☆</span>
+                        </span><br/>
+                        <span className="star-value">{fatherRedFactorLevel}</span>
+                        <span className="star-bar-wrapper">
+                            <input type="range" className="star-bar" id="father-red-factor-star-bar" 
+                                    min={0} max={10} step={1} defaultValue={0} 
+                                    onChange={event => setFatherRedFactorLevel(Number.parseInt(event.target.value))}
+                            />
+                        </span>
+                    </div>
+                    <label htmlFor="mother-red-factor">mother: </label> 
+                    <div id="mother-red-factor" className="red-factor-input">
+                        <select id="mother-red-factor-key" onChange={event => setMotherRedFactorKey(event.target.value as UmaPropertyKey | "")}> 
+                            <option value="">-</option>
+                            {getUmaPropertyKeys().map(key=>
+                                <option value={codeUmaPropertyKey(key)} key={`mother-option-${key}`}>
+                                    {key}
+                                </option>
+                            )}
+                        </select>
+                        <span className="three-stars">
+                            <span className="the-0-star star"></span>
+                            {[1,2,3,4,5,6,7,8,9].map(i => <span className={"the-"+i+"-star star"} key={"mother-star-"+i}>
+                                ★
+                            </span>)
+                            }
+                            <span className="the-10-star star" key="mother-star-10">☆</span>
+                        </span><br/>
+                        <span className="star-value">{motherRedFactorLevel}</span>
+                        <span className="star-bar-wrapper">
+                            <input type="range" className="star-bar" id="mother-red-factor-star-bar" 
+                                    min={0} max={10} step={1} defaultValue={0} 
+                                    onChange={event => setMotherRedFactorLevel(Number.parseInt(event.target.value))}
+                            />
+                        </span>
+                    </div>
+                </div>
                 <label htmlFor="historic-condition-one-rank">search condition 1: </label>
                 <select id="historic-condition-one-rank" onChange={event => setConditionOneRank(Number.parseInt(event.target.value))}>
                     <option value="">-</option>
@@ -140,6 +256,7 @@ const ViewAllHistoricUma = () => {
                     )}
                 </select>
             </div>
+            <button onClick={()=>adoptIngaPreset()}>因果preset</button>
             <div className="uma-row" key="header">
                 <span className="uma-column uma-long-column">name</span>
                 <span className="uma-column">芝</span>
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -27,6 +27,8 @@ test.describe("enjoy basic data browsing", () => {
 });
 
 test.describe("historic list", () => {
+    const starWidth = 15;
+
     const dartEUma = "メイショウドトウ【Dot-o'-Lantern】";
 
     const dartAUmaInSpecialStyle = "ユキノビジン【茶の子雪ん子】";
@@ -34,6 +36,10 @@ test.describe("historic list", () => {
 
     const longATeio = "トウカイテイオー【紫雲の夢見取り】";
     const longBTeio = "トウカイテイオー";
+
+    const dirtGMileC = "キタサンブラック";
+    const dirtGMileALeadC = "アイネスフウジン";
+    const dirtGMileBLeadE = "ビコーペガサス";
 
     test("shortlisting the umas with one condition", async ( {page}, info) => {
         await page.goto("historic/all");
@@ -82,4 +88,133 @@ test.describe("historic list", () => {
         await expect(page.getByText(longBTeio, {exact: true})).toBeVisible({visible:false});
         await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_quadrapleCondition.png", fullPage: true});
     })
+
+    test("shortlisting umas with three conditions and factors", async ({page}, info) => {
+        await page.goto("historic/all");
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible();
+
+        // make it empty with S condition
+        await page.locator("#historic-condition-one-rank").selectOption({label:"S"});
+        await page.locator("#historic-condition-one-key").selectOption({label:"ダート"});
+
+        await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_factors.png", fullPage: true});
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible({visible:false});
+
+        await page.locator("#historic-condition-two-rank").selectOption({label:"A"});
+        await page.locator("#historic-condition-two-key").selectOption({label:"芝"});
+        await page.locator("#historic-condition-three-rank").selectOption({label:"D"});
+        await page.locator("#historic-condition-three-key").selectOption({label:"先行"});
+        await page.locator("#historic-condition-four-rank").selectOption({label:"A"});
+        await page.locator("#historic-condition-four-key").selectOption({label:"マイル"});
+
+        // release S condition. now it requires 芝A, 先行D and マイルA
+        await page.locator("#historic-condition-one-rank").selectOption({label:"G"});
+
+        await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_factors.png", fullPage: true});
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible(); // mile A
+        await expect(page.getByText(dirtGMileC, {exact: true})).toBeVisible({visible: false}); // mile C
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible({visible: false}); // mile B, lead E
+
+        // make マイル +2 by 4 stars
+        await page.locator("#father-red-factor-key").selectOption({label:"マイル"});
+        const leftBar = await page.locator("#father-red-factor-star-bar");
+        if (info.project.name !== "firefox") { // for some reason, firefox tests fail to move sliders or render changes cause by that
+            await leftBar.hover({force: true, position: {x: 0, y: 0}});
+            await page.mouse.down();
+            await leftBar.hover({force: true, position: {x: starWidth * (4+1), y: 0}});
+            await page.mouse.up();
+        } else {
+            const leftBarBox = await leftBar.boundingBox()!;
+            await page.mouse.click(leftBarBox!.x + starWidth * (4+1), leftBarBox!.y);
+        }
+
+        await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_factors.png", fullPage: true});
+        await expect(page.getByText(dirtGMileC, {exact: true})).toBeVisible(); // mile C+2 => A
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible({visible:false}); // mile B+2 => A(not S), but lead E
+
+        await page.locator("#mother-red-factor-key").selectOption({label:"先行"});
+        const rightbar = await page.locator("#mother-red-factor-star-bar");
+        if (info.project.name !== "firefox") { 
+            await rightbar.hover( {force: true, position: {x: 0, y: 0}});
+            await page.mouse.down();
+            await rightbar.hover({force: true, position: {x: starWidth * (1+1), y: 0}});
+            await page.mouse.up();
+        } else {
+            const rightBarBox = await rightbar.boundingBox()!;
+            await page.mouse.click(rightBarBox!.x + starWidth * (4+1), rightBarBox!.y);
+        }
+
+        await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_factors.png", fullPage: true});
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible();
+
+        await page.locator("#mother-red-factor-key").selectOption({label:"長距離"});
+
+        await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_factors.png", fullPage: true});
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible(); // mile A
+        await expect(page.getByText(dirtGMileC, {exact: true})).toBeVisible(); // mile C
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible({visible:false}); // mile A but lead D
+    });
+
+    test("any red factor cannot make a S rank", async({page}, info) => {
+        await page.goto("historic/all");
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible();
+        await expect(page.getByText(dartEUma, {exact: true})).toBeVisible();
+        await expect(page.getByText(dartBUmaInNormalStyle, {exact: true})).toBeVisible();
+        await expect(page.getByText(longATeio, {exact: true})).toBeVisible();
+        await expect(page.getByText(longBTeio, {exact: true})).toBeVisible();
+        await expect(page.getByText(dirtGMileC, {exact: true})).toBeVisible();
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible();
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible();
+
+        // 差し 20 stars
+        await page.locator("#father-red-factor-key").selectOption({label:"差し"});
+        const leftBar = await page.locator("#father-red-factor-star-bar");
+        if (info.project.name !== "firefox") {
+            await leftBar.hover({force: true, position: {x: 0, y: 0}});
+            await page.mouse.down();
+            await leftBar.hover({force: true, position: {x: starWidth * (10+1), y: 0}});
+            await page.mouse.up();
+        } else {
+            const leftBarBox = await leftBar.boundingBox()!;
+            await page.mouse.click(leftBarBox!.x + starWidth * (10+1), leftBarBox!.y);
+        }
+        await page.locator("#mother-red-factor-key").selectOption({label:"差し"});
+        const rightbar = await page.locator("#mother-red-factor-star-bar");
+        if (info.project.name !== "firefox") { 
+            await rightbar.hover( {force: true, position: {x: 0, y: 0}});
+            await page.mouse.down();
+            await rightbar.hover({force: true, position: {x: starWidth * (10+1), y: 0}});
+            await page.mouse.up();
+        } else {
+            const rightBarBox = await rightbar.boundingBox()!;
+            await page.mouse.click(rightBarBox!.x + starWidth * (10+1), rightBarBox!.y);
+        }
+
+        // even 20 stars should not let ranks reach S 
+        await page.locator("#historic-condition-one-rank").selectOption({label:"S"});
+        await page.locator("#historic-condition-one-key").selectOption({label:"差し"});
+
+        await page.screenshot({path: "tests/screenshots/histolicList/"+info.project.name+"_A_cap_empty.png", fullPage: true});
+        await expect(page.getByText(dartEUma, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dartAUmaInSpecialStyle, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dartBUmaInNormalStyle, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(longATeio, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(longBTeio, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dirtGMileC, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible({visible:false});
+
+        // but some umas should reach A by the stars
+        await page.locator("#historic-condition-one-rank").selectOption({label:"A"});
+
+        //   (no screenshot here to preserve the empty shot)
+        await expect(page.getByText(dartEUma, {exact: true})).toBeVisible();
+        await expect(page.getByText(dartAUmaInSpecialStyle, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dartBUmaInNormalStyle, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(longATeio, {exact: true})).toBeVisible();
+        await expect(page.getByText(longBTeio, {exact: true})).toBeVisible();
+        await expect(page.getByText(dirtGMileC, {exact: true})).toBeVisible();
+        await expect(page.getByText(dirtGMileALeadC, {exact: true})).toBeVisible({visible:false});
+        await expect(page.getByText(dirtGMileBLeadE, {exact: true})).toBeVisible();
+    });      
 })
