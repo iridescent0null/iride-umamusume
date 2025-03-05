1e0eb863c39f6e96e8f0c37b8a19af12c73fbadf
iridescent0null <mitome19+dev@gmail.com>
2025-03-05 17:37:05 +0900

    fix the script (position of the test script)
    fix the sed command for AWS test
    
    change the script timming (pre -> post)
    
    fix the path
    
    make the path absolute

--- tests/pre.sh
+++ tests/pre.sh
@@ -1,5 +1,3 @@
 # fixing the FQDN in the test script 
-sed -i s/localhost/ec2-57-182-177-197.ap-northeast-1.compute.amazonaws.com/ /home/ubuntu/test/playwright.config.ts
-sudo su -
-cd /home/ubuntu/test/
-npx playwright test test.spec.ts 
+sed -i s/localhost/ec2-57-182-177-197.ap-northeast-1.compute.amazonaws.com/ test.spec.ts
+npx playwright test /home/ubuntu/test/tests/test.spec.ts 
