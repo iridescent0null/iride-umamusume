3f58faa9799dd903778d15e54c5ab66e3cdc043e
iridescent0null <mitome19+dev@gmail.com>
2025-03-06 08:34:22 +0900

    add cd

--- tests/pre.sh
+++ tests/pre.sh
@@ -1,3 +1,5 @@
 # fixing the FQDN in the test script 
 sed -i s/localhost/ec2-57-182-177-197.ap-northeast-1.compute.amazonaws.com/ /home/ubuntu/test/playwright.config.ts
-npx playwright test /home/ubuntu/test/tests/test.spec.ts 
+sudo su -
+cd /home/ubuntu/test/
+npx playwright test test.spec.ts 
