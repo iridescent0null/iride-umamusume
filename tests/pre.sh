a5cb16deb34f880797e155cd71e7e34b5ae80829
iridescent0null <mitome19+dev@gmail.com>
2025-03-05 17:50:22 +0900

    fix the sed command for AWS test

--- tests/pre.sh
+++ tests/pre.sh
@@ -1,2 +1,2 @@
 # fixing the FQDN in the test script 
-sed -i s/localhost/ec2-57-182-177-197.ap-northeast-1.compute.amazonaws.com/ tests/test.spec.ts
+sed -i s/localhost/ec2-57-182-177-197.ap-northeast-1.compute.amazonaws.com/ `pwd`/playwright.config.ts
